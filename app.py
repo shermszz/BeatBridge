bb19c49e06d6e7d2fc183dcb1746e9daffb1b9b6
Jw <hjw@192.168.1.6>
2025-05-23 11:33:53 +0800

    Added static folder, rename python file to app.py and added flask code to it, edited layout.html and added home.html

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, session, redirect
+from flask_session import Session
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Add SQL database here
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
