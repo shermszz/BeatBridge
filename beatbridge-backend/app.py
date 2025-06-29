d603672b68169805c3c784219dd08df47e4e2cc9
Jw <hjw@192.168.1.5>
2025-06-29 21:35:09 +0800

    Changes

--- beatbridge-backend/app.py
+++ beatbridge-backend/app.py
@@ -40,6 +40,7 @@ ALLOWED_ORIGINS = [
 
 # Configure application
 app = Flask(__name__)
+app.config['SESSION_COOKIE_NAME'] = 'session'
 CORS(app, origins=ALLOWED_ORIGINS, supports_credentials=True)
 
 # Database configuration
